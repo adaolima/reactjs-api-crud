e6f25b8f9209f8c0391562b2e4551d1ca25911d0
Adao Lima <adaoolima@gmail.com>
2020-03-11 17:31:55 -0300

    Testing navbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
-import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Nav from "react-bootstrap/Nav";
+import { Link } from "react-router-dom";
+import Routes from "./routes";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <h1>Cadastre usuários</h1>
+        <Nav variant="pills" defaultActiveKey="/home">
+          <Nav.Item>
+            <Nav.Link href="/new">New</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </header>
+      <Routes />
     </div>
   );
 }
