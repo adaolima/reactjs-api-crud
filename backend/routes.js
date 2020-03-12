ef3e425cb332209ab8250e59c0a9358f5a23c697
Adao Lima <adaoolima@gmail.com>
2020-03-12 15:27:35 -0300

    Completed routes for CRUD

--- backend/routes.js
+++ backend/routes.js
@@ -6,5 +6,7 @@ const routes = express.Router();
 
 routes.get("/api/v1/users", UserController.read);
 routes.post("/api/v1/users", UserController.create);
+routes.put("/api/v1/users", UserController.update);
+routes.delete("/api/v1/users", UserController.delete);
 
 module.exports = routes;
