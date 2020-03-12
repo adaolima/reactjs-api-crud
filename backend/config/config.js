99c5f47ffb07b0f1b3212ad6924b481c3c32883f
Adao Lima <adaoolima@gmail.com>
2020-03-12 14:00:49 -0300

    Load dotenv variables

---
+++ backend/config/config.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.DB_HOST,
+    post: process.env.DB_PORT,
+    dialect: "mysql",
+    operatorsAliases: false
+  }
+};
