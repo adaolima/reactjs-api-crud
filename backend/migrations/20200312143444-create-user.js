72e89cfff1af43cd844bbc679d342cc5b37a28d3
Adao Lima <adaoolima@gmail.com>
2020-03-12 13:28:22 -0300

    generated migration

---
+++ backend/migrations/20200312143444-create-user.js
@@ -0,0 +1,42 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("User", {
+      user_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      full_name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      birth_date: {
+        type: Sequelize.DATEONLY
+      },
+      cpf: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("User");
+  }
+};
