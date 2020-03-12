8a33bfceab68d544379f751d2086cbcf4f58a577
Adao Lima <adaoolima@gmail.com>
2020-03-12 02:29:40 -0300

    Added a table as strucuture base for user list

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,7 +1,54 @@
 import React from "react";
-
-// import { Container } from './styles';
+import Helmet from "react-helmet";
+import { Table } from "react-bootstrap";
+import Paginate from "../../components/Paginate";
+// import api from "../../services/api";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <>
+      <Helmet>
+        <title>Dashboard</title>
+      </Helmet>
+      <div className="mt-4">
+        <h2 className="my-4">Lista de usuários cadastrados</h2>
+        <Table striped bordered hover responsive="sm">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Nome</th>
+              <th>Sobrenome</th>
+              <th>E-mail</th>
+              <th>Telefone</th>
+              <th>CPF</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+            </tr>
+          </tbody>
+        </Table>
+        <div className="py-4 d-flex justify-content-center">
+          <Paginate />
+        </div>
+      </div>
+    </>
+  );
 }
