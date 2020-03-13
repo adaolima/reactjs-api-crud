4c352b2974833daa4a08def4ac2cb148055caf40
Adao Lima <adaoolima@gmail.com>
2020-03-13 12:42:04 -0300

    Added routes to do login

--- backend/routes.js
+++ backend/routes.js
@@ -8,5 +8,7 @@ routes.get("/api/v1/users", UserController.read);
 routes.post("/api/v1/users", UserController.create);
 routes.put("/api/v1/users", UserController.update);
 routes.delete("/api/v1/users", UserController.delete);
+routes.post("/api/v1/login", UserController.authenticate);
+routes.delete("/api/v1/logout", UserController.logout);
 
 module.exports = routes;
