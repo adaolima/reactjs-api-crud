cb03b688eb3cb9aad9b82a04e6d339a5cc65f896
Adao Lima <adaoolima@gmail.com>
2020-03-13 12:39:56 -0300

    Cleaned server

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,23 @@
 require("dotenv").config();
 const express = require("express");
-// const logger = require("morgan");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+
 const cors = require("cors");
-const AuthMiddleware = require("./middleware/auth-middleware");
 const routes = require("./routes");
 
 const http = require("http");
 
 const app = express();
 
-app.use(express.logger("dev"));
+app.use(logger("dev"));
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.cookieParser());
-app.use(AuthMiddleware);
-
-//Models
-var models = require("./models");
-//Sync Database
-models.sequelize
-  .sync()
-  .then(function() {
-    console.log("Nice! Database looks fine");
-  })
-  .catch(function(err) {
-    console.log(err, "Something went wrong with the Database Update!");
-  });
+app.use(cookieParser());
 
 app.use(routes);
-
-app.get("*", (req, res) => {
-  res.status(200).send({
-    message: "All things start here..."
-  });
-});
 
 const port = process.env.API_PORT || 8000;
 
