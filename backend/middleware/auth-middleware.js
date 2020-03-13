8790b2288e1374e0690cbbd28e2d334b3bb7f4fd
Adao Lima <adaoolima@gmail.com>
2020-03-13 12:42:42 -0300

    Check if use is authorizeted

--- backend/middleware/auth-middleware.js
+++ backend/middleware/auth-middleware.js
@@ -1,16 +1,25 @@
 const { User, AuthToken } = require("../models");
 
 module.exports = async (req, res, next) => {
-  const tokens = req.cookies.auth_token || req.headers.authorization;
-  if (token) {
-    const authToken = await AuthToken.find({
-      where: { token },
-      include: User
-    });
-
-    if (authToken) {
-      req.user = authToken.User;
+  const token = req.cookies.auth_token || req.headers.authorization;
+  const user_id = req.headers.user_id;
+  console.log(token);
+  console.log(user_id);
+  try {
+    if (token) {
+      const authToken = await AuthToken.findOne({
+        where: { token, user_id }
+      });
+      // console.log(req.cookies.auth_token);
+      if (authToken) {
+        //req.user = authToken.User;
+        return res.json("Usuário autorizado");
+      }
+    } else {
+      res.json("Não Autorizado");
     }
+    next();
+  } catch (error) {
+    res.json("Não foi possivel se conectar.");
   }
-  next();
 };
